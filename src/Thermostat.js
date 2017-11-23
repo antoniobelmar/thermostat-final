8b911a8be589f72ff27914a6dd8270a3cf7ff1df
antonio <antoniobelmardacosta@gmail.com>
2017-11-23 23:11:23 +0000

    Add ; to return statements

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -9,25 +9,25 @@ function Thermostat(){
 // TEMPERATURE GETTER
 
 Thermostat.prototype.getCurrentTemperature = function(){
-  return this.temperature
+  return this.temperature;
 }
 
 // RETURN BOOLEAN
 
 Thermostat.prototype.isMinimumTemperature = function(){
-  return this.temperature <= this.MINIMUM
-}
+  return this.temperature <= this.MINIMUM;
+};
 
 Thermostat.prototype.isPowerSavingModeOn = function(){
-  return this.powerSavingMode
-}
+  return this.powerSavingMode;
+};
 
 Thermostat.prototype.isMaximumTemperature = function(){
   if (this.isPowerSavingModeOn()) {
-    return this.temperature >= this.MAX_PSON
+    return this.temperature >= this.MAX_PSON;
   }
-  return this.temperature >= this.MAX_PSOFF
-}
+  return this.temperature >= this.MAX_PSOFF;
+};
 
 // CHANGE TEMPERATURE
 
@@ -35,38 +35,38 @@ Thermostat.prototype.up = function(){
   if (this.isMaximumTemperature()) {
     return;
   }
-  this.temperature += 1
+  this.temperature += 1;
 }
 
 Thermostat.prototype.down = function(){
   if (this.isMinimumTemperature()) {
     return;
   }
-  this.temperature -= 1
-}
+  this.temperature -= 1;
+};
 
 Thermostat.prototype.reset = function(){
-  this.temperature = 20
-}
+  this.temperature = 20;
+};
 
 // POWER SAVING SWITCHES
 
 Thermostat.prototype.turnPowerSavingModeOff = function(){
-  this.powerSavingMode = false
-}
+  this.powerSavingMode = false;
+};
 
 Thermostat.prototype.turnPowerSavingModeOn = function(){
-  this.powerSavingMode = true
-}
+  this.powerSavingMode = true;
+};
 
 // ENERGY USAGE
 
 Thermostat.prototype.energyUsage = function(){
   if (this.getCurrentTemperature() < 18) {
-    return 'low-usage'
+    return 'low-usage';
   } else if (this.getCurrentTemperature() < 25) {
-    return 'medium-usage'
+    return 'medium-usage';
   } else {
-    return 'high-usage'
+    return 'high-usage';
   }
-}
+};
